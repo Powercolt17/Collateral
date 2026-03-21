260f8264bd7ffa4c251fd6f50506271ad3727602
Powercolt17 <98866572+Powercolt17@users.noreply.github.com>
2026-03-20 22:53:09 -0500

    Update Header.js

--- FRONTEND/src/components/Header.js
+++ FRONTEND/src/components/Header.js
@@ -655,9 +655,8 @@ export function renderHeader(currentRoute) {
                             <button onclick="window.router.navigate('/profile')">Profile</button>
                             <button onclick="window.router.navigate('/contracts')">Active</button>
                             <button onclick="window.router.navigate('/referrals')">Referrals</button>
-                            <button onclick="window.router.navigate('/docs')">Docs</button>
-
                             <button onclick="window.router.navigate('/funding')">Account Capital</button>
+                            <button onclick="window.router.navigate('/docs')">Docs</button>
                             <button onclick="window.app.handleSignOut()" class="signout">SIGN OUT</button>
                         </div>
                     </div>
